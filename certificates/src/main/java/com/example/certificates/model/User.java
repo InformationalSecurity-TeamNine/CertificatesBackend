7efed887d84edafaec1e63316d56f1fca360f359
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-03-18 11:09:30 +0100

    Added enums and entity to models

--- certificates/src/main/java/com/example/certificates/model/User.java
+++ certificates/src/main/java/com/example/certificates/model/User.java
@@ -1,4 +1,22 @@
 package com.example.certificates.model;
 
-public class User {
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
