90c2eb850d8255d09e36eec3018f4af3d3d69eb6
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-03-18 10:54:21 +0100

    Created Certificates project

---
+++ certificates/src/main/java/com/example/certificates/CertificatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.certificates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesApplication.class, args);
+	}
+
+}
