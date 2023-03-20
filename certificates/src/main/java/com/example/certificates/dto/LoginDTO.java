b40fdc21d55453ad67feeda48272ff8c1ae7c0ce
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-03-20 20:49:41 +0100

    Added regex to login dto

--- certificates/src/main/java/com/example/certificates/dto/LoginDTO.java
+++ certificates/src/main/java/com/example/certificates/dto/LoginDTO.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -14,12 +17,13 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class LoginDTO {
 
-    @NotNull
+    @Email(message = "{format}")
     @NotEmpty(message = "{required}")
+    @Length(max=40, message = "{maxLength}")
     private String email;
 
-    @NotNull
     @NotEmpty(message = "{required}")
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "{invalidPasswordFormat}")
     private String password;
 
 }
