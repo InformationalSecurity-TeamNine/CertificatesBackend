2a9b69a7f40b0b2c42d5c5ec8f6f1c56b90330b4
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-03-23 18:41:22 +0100

    Added get all

--- certificates/src/main/java/com/example/certificates/service/CertificateService.java
+++ certificates/src/main/java/com/example/certificates/service/CertificateService.java
@@ -1,7 +1,15 @@
 package com.example.certificates.service;
 
+import com.example.certificates.model.Certificate;
+import com.example.certificates.model.Paginated;
+import com.example.certificates.service.interfaces.ICertificateService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CertificateService {
+public class CertificateService implements ICertificateService {
+    
+    @Override
+    public Paginated<Certificate> getAll() {
+        return null;
+    }
 }
--- certificates/src/main/java/com/example/certificates/service/interfaces/ICertificateService.java
+++ certificates/src/main/java/com/example/certificates/service/interfaces/ICertificateService.java
@@ -1,5 +1,9 @@
 package com.example.certificates.service.interfaces;
 
+import com.example.certificates.model.Certificate;
+import com.example.certificates.model.Paginated;
+
 public interface ICertificateService {
 
+    Paginated<Certificate> getAll();
 }
