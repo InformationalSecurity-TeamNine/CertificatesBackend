360288e64628eb2ffbc39c787d56c625ca0e7e38
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-03-27 21:15:53 +0200

    Updated model

--- certificates/src/main/java/com/example/certificates/model/Certificate.java
+++ certificates/src/main/java/com/example/certificates/model/Certificate.java
@@ -1,9 +1,12 @@
 package com.example.certificates.model;
 
+import com.example.certificates.enums.CertificateStatus;
+import com.example.certificates.enums.CertificateType;
 import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -19,5 +22,27 @@ public class Certificate implements Serializable {
     private Long id;
 
     @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
-    private User owner;
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.REFRESH}, fetch = FetchType.LAZY)
+    private Certificate issuingCertificate;
+
+    private String serialNumber;
+
+    private String signatureAlgorithm;
+
+    private LocalDateTime validFrom;
+
+    private LocalDateTime validTo;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private CertificateStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private CertificateType type;
+
+
 }
+
