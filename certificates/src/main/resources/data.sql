fb9afcbe38f27837e0ae55e9458388cae1c17a4d
Bogdan <bogdanjanosevic@gmail.com>
2023-05-15 16:23:05 +0200

    my phone number was wrong

--- certificates/src/main/resources/data.sql
+++ certificates/src/main/resources/data.sql
@@ -5,7 +5,7 @@ insert into Users(email, password, name, surname, telephone_number, role, is_ema
 values ('nebojsa@gmail.com', '$2a$12$HXFqjPtx.FE7OP530tCzQOMibvJx8RfFzzUJKoCB4wo7ugTTSReS6', 'Nebojsa', 'Vuga', '069512512', 'BASIC_USER', true, '2023-03-23 12:55:58.588198');
 
 insert into Users(email, password, name, surname, telephone_number, role, is_email_confirmed, last_time_password_changed)
-values ('bogdan@gmail.com', '$2a$12$2AC05eNYundzzLYOhY14w.cZE/UBIIr4xZZ3N42.Gxd8DTlcggliW', 'Bogdan', 'Janosevic', '0659715120', 'BASIC_USER', true, '2023-05-13 12:55:58.588198');
+values ('bogdan@gmail.com', '$2a$12$2AC05eNYundzzLYOhY14w.cZE/UBIIr4xZZ3N42.Gxd8DTlcggliW', 'Bogdan', 'Janosevic', '+381659715120', 'BASIC_USER', true, '2023-05-13 12:55:58.588198');
 
 insert into Users(email, password, name, surname, telephone_number, role, is_email_confirmed, last_time_password_changed)
 values ('dusan@gmail.com', '$2a$12$pOWqvnMcnitY5KihwitVd.SKXLTqQq7uybnKYv3161mYmKqKCglOS', 'Dusan', 'Bibin', '06421412', 'BASIC_USER', true, '2023-03-23 12:55:58.588198');
