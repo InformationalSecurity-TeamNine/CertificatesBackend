5f1c25e65642c978606eb1dc75b90e13ddc3f68e
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-05-27 13:38:11 +0200

    Added generated root cert

--- certificates/src/main/resources/data.sql
+++ certificates/src/main/resources/data.sql
@@ -13,3 +13,8 @@ values ('dusan@gmail.com', '$2a$12$pOWqvnMcnitY5KihwitVd.SKXLTqQq7uybnKYv3161mYm
 insert into Users(email, password, name, surname, telephone_number, role, is_email_confirmed, last_time_password_changed)
 values ('mirko@gmail.com', '$2a$12$VfsULMyyWkkn64G70ERZ3.lrXnKTfrft6VQwYJ1PZk59DkJp0yuWq', 'Mirko', 'Babic', '06444421', 'BASIC_USER', false,'2023-03-23 12:55:58.588198');
 
+insert into certificate_request(id, certificate_type, reason, status, time, issuer_id, parent_certificate_id)
+values (1, 'ROOT', null, 'ACCEPTED', '2027-12-27 12:29:00.00000', 1, null);
+
+insert into certificate(id, public_key, serial_number, signature_algorithm, status, type, valid_from, valid_to, issuing_certificate_id, user_id)
+values (1, '24106', '48f1a809-a808-4c19-87e0-9c5203013342', 'SHA256WithRSAEncryption', 'VALID', 'ROOT',  '2023-05-26 12:29:00.00000', '2027-12-27 12:29:00.00000', null, 1);
